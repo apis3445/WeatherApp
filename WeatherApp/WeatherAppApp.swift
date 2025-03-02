7b162a9caf0da0a893cff66b261d06efa868917c
abigailarmijohernandez <abi1521@hotmail.com>
2025-03-01 19:29:03 -0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Abigail Armijo Hernández on 01/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
